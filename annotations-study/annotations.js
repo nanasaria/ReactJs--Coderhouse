f525b60e6680f994e0b251d80af1d49cd1804b77
Nayara N Soares <nnasaria@mundolivre.digital>
2025-05-22 20:21:10 -0300

    Anotações

--- annotations-study/annotations.js
+++ annotations-study/annotations.js
@@ -110,4 +110,248 @@ Características dos Componentes Containers:
 - Comunicação entre componentes: Gerenciam como os componentes filhos interagem entre si e com
 a fonte de dados.
 
+Ciclo de Vida de Componentes
+
+Um componente passa por três fases-chave:
+
+1. Montagem: Componente é criado e inserido no DOM. Nesse
+momento podem ser executadas as configurações iniciais, como
+a obtenção de dados de APIs ou a configuração de estados e
+props.
+
+2. Atualização: Ocorre sempre que o componente sofre mudanças, 
+seja por uma atualização de seus props ou estados. Nessa
+fase, o componente é re-renderizado para refletir as
+mudanças.
+
+3. Desmontagem: É a fase final do clico de vida de um
+componente, quando ele é removido do DOM. 
+
+Children e Props
+
+A propriedade children permite projetar ou transcluir um ou 
+mais componentes dentro de outro, facilitando a criação de
+estruturas de componentes mais complexas e reutilizáveis.
+Propriedade usada para adicionar um conteúdo sem que o 
+elemento pai o conheça.
+
+1. Uso de Children 
+No componente App, o componente Layout recebe conteúdo entre
+suas tags e o conteúdo é projetado dentro do componente filho 
+através da propriedade children.
+
+2. Injeção automática
+O React injeta automaticamente o conteúdo passado entre as tags
+de um componente nas props do componente filho.
+
+3. Versatilidade de Children
+A propriedade children pode manipular um único elemento ou 
+um array de elementos, dependendo da quantidade de conteúdo 
+que é passada para o componente.
+
+HOOKS
+
+useRef
+O hook useRef permite criar referências mutáveis, ou seja,
+objetos que mantêm seu valor ao longo dos renderizados
+de um componente sem provocar re-renderizações. 
+
+O uso é útil ao trabalhar com bibliotecas de terceiros que 
+exigem manipulação direta do DOM ou quando é necessário
+manter um valor persistente, como um contador de renderizações
+ou uma referência a um temporizador, sem causar efeitos 
+colaterais indesejados.
+
+useEffect
+O hook useEffect é utilizado para lidar com efeitos colaterais
+e sincronizar nossos componentes com sistemas externos de 
+maneira eficiente.
+Rodamos efeitos sempre que houver alteração em um estado.
+Passamos uma referência para o useEffect, para toda
+vez que o estado de algo mudar, aplicar o efeito.
+O useEffect remonta o componente toda vez que é chamado.
+
+useEffect(() => {}, [])
+
+O 2° parâmetro da função é chamado de array de dependência.
+Há 3 cenários:
+- Array vazio: Executa 1 única vez.
+- Se não passar o array de dependência, executa toda vez que
+o componente for renderizado.
+- Variável para executar toda vez que o estado mudar.
+
+Podemos gerar um loop infinito, uma boa prática é sempre passar
+uma dependência.
+
+useState
+O hook useState permite adicionar e gerenciar o estado de
+componentes funcionais. Recebe como argumento o valor inicial 
+do estado e retorna um array com dois elementos: O estado
+atual e uma função para atualizá-lo.
+As atualizações do estado são assíncronas e o React agrupa
+múltiplas mudanças de estado em uma única atualização para
+otimizar o desempenho.
+
+Geração de Listas com o Método Map
+O método map aceita uma função de callback como argumento,
+que é executada uma vez para cada elemento do array, 
+retornando um novo array com os resultados da
+aplicação da função a cada um dos elementos originais.
+
+const App = () => {
+    const items = ["Maçã", "Banana", "Cereja"]
+    
+    return (
+        <ul>
+        {items.map((item, index) => (
+            li key={index}>{item}</li>
+        ))}
+        </ul>
+    )
+}
+
+Nesse exemplo, o map itera sobre o array items, gerando
+um novo array de elementos <li> que são renderizados 
+dentro da lista <ul>. Cada elemento possui uma key que 
+ajuda a identificar cada elemento.
+
+A Key é essencial quando se rederizam listas no React.
+Ela permite que o React identifique e acompanhe cada
+elemento durante mudanças de estado ou atualizações.
+Se os elementos do array têm um identificados único,
+o ideal é utilizá-lo como key.
+
+const App = () => {
+  const users = [
+    { id: 1, name: 'João' },
+    { id: 2, name: 'Ana' },
+    { id: 3, name: 'Carlos' },
+  ];
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+Criação de Componentes Dinâmicos
+O método map também permite a criação de componentes dinâmicos
+e reutilizáveis.
+
+const User = ({ name, status }) => (
+  <li>
+    <span>{name}</span>
+    <small>{status}</small>
+  </li>
+);
+
+const App = () => {
+  const users = [
+    { id: 1, name: 'João', status: 'Online' },
+    { id: 2, name: 'Ana', status: 'Offline' },
+  ];
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <User key={user.id} name={user.name} status={user.status} />
+      ))}
+    </ul>
+  );
+};
+
+É criado um componente User que recebe parâmetros como props,
+e é reutilizado dentro do map para gerar cada elemento da
+lista.
+
+Paradigmas de Informação
+
+Modelo Cliente-Servidor
+Dois atores participam: Cliente e servidor e os dois se 
+comunicam. O Cliente faz requisições e o servidor
+entrega respostas.
+
+Vantagem do modelo Cliente-Servidor
+Oferece total controle ao Cliente sobre quando iniciar as
+solicitações, permitindo uma comunicação clara e previsível.
+
+Desvantagem do modelo Cliente-Servidor
+Depende do Cliente para iniciar a comunicação.
+
+Modelo Push
+Esse modelo inverte a lógica Cliente-Servidor. Nesse modelo, o 
+Servidor é quem inicia a comunicação.
+
+Funcionamento do Modelo Push
+No Modelo Push, o Servidor inicia a comunicação. O Cliente 
+se inscreve em um serviço e o Servidor envia informações de
+forma proativa, sem esperar uma solicitação prévia do 
+Cliente. Este modelo é ideal para notificações e alertas, 
+mantendo os usuários informados em tempo real, como em 
+aplicativos de mensagens ou notificações móveis.
+
+Vantagens
+Mantém os usuários engajados ao enviar atualizações de
+forma proativa.
+
+Desvantagens
+Consome mais recursos. Requer manutenção constante.
+
+Modelo Polling
+O Cliente assume um papel ativo ao consultar periodicamente
+o Servidor para verificar se há novos dados ou atualizações.
+
+Funcionamento do Modelo Polling
+O Cliente envia solicitações periódicas ao Servidor para 
+verificar se há novas atualizações. O Servidor responde 
+com os dados disponíveis ou indica que não há mudanças. 
+
+Vantagens
+Fácil de configurar. Não exige conexão constante com o Servidor.
+Permite que o Cliente controle a frequência das solicitações.
+
+Desvantagens
+Pode ser ineficiente devido a solicitações repetitivas.
+Desperdiça recursos e largura de banda.
+
+HTTP
+Protocolo de comunicação.
+
+Componentes de uma Requisição HTTP:
+URL (Uniform Resource Locator): Define o endereço do recurso solicitado.
+Verbo ou Método: Ação que o cliente deseja realizar (GET, POST, PUT, DELETE, etc).
+Headers (cabeçalhos): Incluem informações adicionais sobre 
+a requisição, como o tipo de conteúdo aceito ou dados de 
+autenticação.
+Parâmetros: Fornecem detalhes adicionais sobre a solicitação, 
+seja através de URL (query params) ou no corpo (body params).
+
+Query Params
+Esses parâmetros são adicionados ao final da URL após um 
+ponto de interrogação ? e são usados para enviar dados 
+opcionais ao servidor.
+
+Estrutura: key=value, separados por & se houver mais de um.
+
+Path Parameters (Parâmetros de Rota):
+São integrados diretamente na rota da URL e são mais comuns
+ao acessar um recurso específico.
+
+Uso prático: Acessar os detalhes de um usuário específico 
+por meio de seu ID.
+
+Exemplo:
+arduino https://www.exemplo.com/usuarios/123
+
+
+APIs e Fetch
+Uma API, ou Interface de Programação de Aplicações, é um 
+conjunto de definições e protocolos que permitem a 
+comunicação entre diferentes aplicações. As APIs facilitam 
+que uma aplicação acesse funções ou dados específicos de 
+outra sem a necessidade de conhecer sua implementação 
+interna, promovendo uma integração eficiente e estruturada.
 */
